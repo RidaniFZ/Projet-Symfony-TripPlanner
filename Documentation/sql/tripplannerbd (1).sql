436b4ef48a607cb70d8abc87f6b9433b0ce62fff
RidaniFZ <ridani.fz@gmail.com>
2020-04-24 12:45:01 +0200

    sql file

--- Documentation/sql/tripplannerbd (1).sql
+++ Documentation/sql/tripplannerbd (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 24 avr. 2020 à 10:45
+-- Généré le : ven. 24 avr. 2020 à 12:43
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.1
 
@@ -22,20 +22,6 @@ SET time_zone = "+00:00";
 -- Base de données : `tripplannerbd`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `activit`
---
-
-CREATE TABLE `activit` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prix` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `activit`
 --
@@ -51,19 +37,6 @@ INSERT INTO `activit` (`id`, `nom`, `type`, `description`, `prix`) VALUES
 (8, 'Tour dans un Yacht', 'Plien Aire', 'Excursions en bateau et sports nautiques, Activités de plein air, Plus', '55'),
 (9, 'Camping The Molen', 'Plien Aire', 'Camping situé au calme, à 100 m de la plage Ste-Anne et à 1 km des magasins', '45');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `groupe`
---
-
-CREATE TABLE `groupe` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `admin_groupe_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `groupe`
 --
@@ -76,19 +49,6 @@ INSERT INTO `groupe` (`id`, `nom`, `description`, `admin_groupe_id`) VALUES
 (7, 'les coupins', 'le groupe est crée entre coupins.', 10),
 (8, 'Best Sisters Forever', 'Groupe for my sisters.', 8),
 (9, 'Animals lovers', 'le groupe est crée pour les gens qui s’intéresse aux animaux.', 8);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `hebergement`
---
-
-CREATE TABLE `hebergement` (
-  `id` int(11) NOT NULL,
-  `adress` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prix_par_nuit` decimal(10,0) NOT NULL,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `hebergement`
@@ -104,17 +64,6 @@ INSERT INTO `hebergement` (`id`, `adress`, `prix_par_nuit`, `type`) VALUES
 (14, '3 Rue Arthur Groussier 75010 Paris France', '161', 'La Planque Hotel'),
 (15, 'Godefriduskaai 99, 2000 Antwerpen', '250', 'Jachthaven Antwerpen Willemdok'),
 (16, 'Houtum 39 2460  Kasterlee Belgique', '28', 'Camping');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `migration_versions`
---
-
-CREATE TABLE `migration_versions` (
-  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `migration_versions`
@@ -136,24 +85,6 @@ INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
 ('20200418185216', '2020-04-18 18:52:30'),
 ('20200421123839', '2020-04-21 12:39:00');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `trip`
---
-
-CREATE TABLE `trip` (
-  `id` int(11) NOT NULL,
-  `destination` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date_debut` datetime NOT NULL,
-  `date_fin` datetime NOT NULL,
-  `trip_groupe_id` int(11) NOT NULL,
-  `trip_hebergement_id` int(11) NOT NULL,
-  `trip_admin_id` int(11) NOT NULL,
-  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `trip`
 --
@@ -164,17 +95,6 @@ INSERT INTO `trip` (`id`, `destination`, `date_debut`, `date_fin`, `trip_groupe_
 (11, 'Paris', '2020-08-18 00:00:00', '2020-08-20 00:00:00', 7, 14, 10, '71251325350c907ea8afe558ac52a23b.jpeg', 'Paris, capitale de la France, est une grande ville européenne et un centre mondial de l\'art, de la mode, de la gastronomie et de la culture. Son paysage urbain du XIXe siècle est traversé par de larges boulevards et la Seine.'),
 (12, 'Anvers', '2020-06-28 00:00:00', '2020-06-30 00:00:00', 6, 15, 10, '81ca362e22a228960c6d51e68340184e.jpeg', 'Anvers est une ville portuaire belge située sur l\'Escaut, dont l\'histoire remonte au Moyen Âge. Au centre-ville, le quartier des diamantaires vieux de plusieurs siècles est le repère de milliers de marchands, tailleurs et polisseurs de diamants.'),
 (13, 'Anvers', '2020-08-01 00:00:00', '2020-08-03 00:00:00', 9, 16, 8, '98d08365e6fab8b3dfbe1d7afb9d3142.jpeg', 'Anvers est une ville portuaire belge située sur l\'Escaut, dont l\'histoire remonte au Moyen Âge. Au centre-ville, le quartier des diamantaires vieux de plusieurs siècles est le repère de milliers de marchands, tailleurs et polisseurs de diamants.');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `trip_activit`
---
-
-CREATE TABLE `trip_activit` (
-  `trip_id` int(11) NOT NULL,
-  `activit_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `trip_activit`
@@ -187,25 +107,6 @@ INSERT INTO `trip_activit` (`trip_id`, `activit_id`) VALUES
 (12, 8),
 (13, 9);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pays` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `adress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 --
 -- Déchargement des données de la table `user`
 --
@@ -216,17 +117,6 @@ INSERT INTO `user` (`id`, `email`, `roles`, `password`, `nom`, `prenom`, `pays`,
 (10, 'ayoub@gmail.com', '[]', '$argon2id$v=19$m=65536,t=4,p=1$UjUvcGZvZWJpQ3VmNENnRw$FiJAnMvG55a5VvDpKgHt2XMRfzTR0qch9Nrn8I4GZms', 'Ridani', 'Ayoub', 'France', 'Metz', 'b2b6f9d6c5ace19bbd5321ebf7efbcbc.jpeg', 'goin all over the world and descovering new places and getting in touch with new ppl.'),
 (11, 'ridani@gmail.com', '[]', '$argon2id$v=19$m=65536,t=4,p=1$NmdHZnQuNURhMnRsRW15dg$PRcO3sOIcRkwxL1WkM58n868fHbyT/NbLUySr5Hf/2k', 'hello', 'hello', 'hello', 'hello', '4a82c60c327efa9a797ddfb8bb502c0c.jpeg', 'hello'),
 (12, 'ridani.fatima@gmail.com', '[]', '$argon2id$v=19$m=65536,t=4,p=1$T1RMeENVOGp1QXBHVXVGMg$lKktnrYShv4AAslcuvSBz/vw0jNt9axRy3XUri6tBeM', 'hello', 'hello', 'hello', 'hello', 'd2da0e53729cdab12317d0b59290af80.jpeg', 'hello');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user_groupe`
---
-
-CREATE TABLE `user_groupe` (
-  `user_id` int(11) NOT NULL,
-  `groupe_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `user_groupe`
@@ -239,133 +129,6 @@ INSERT INTO `user_groupe` (`user_id`, `groupe_id`) VALUES
 (9, 8),
 (9, 9),
 (10, 5);
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `activit`
---
-ALTER TABLE `activit`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `groupe`
---
-ALTER TABLE `groupe`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_4B98C2193B230BE` (`admin_groupe_id`);
-
---
--- Index pour la table `hebergement`
---
-ALTER TABLE `hebergement`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `migration_versions`
---
-ALTER TABLE `migration_versions`
-  ADD PRIMARY KEY (`version`);
-
---
--- Index pour la table `trip`
---
-ALTER TABLE `trip`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_7656F53B675A74DB` (`trip_groupe_id`),
-  ADD KEY `IDX_7656F53B6236ABB4` (`trip_hebergement_id`),
-  ADD KEY `IDX_7656F53BCC7AB100` (`trip_admin_id`);
-
---
--- Index pour la table `trip_activit`
---
-ALTER TABLE `trip_activit`
-  ADD PRIMARY KEY (`trip_id`,`activit_id`),
-  ADD KEY `IDX_F1762FD9A5BC2E0E` (`trip_id`),
-  ADD KEY `IDX_F1762FD9BA71FC0C` (`activit_id`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
-
---
--- Index pour la table `user_groupe`
---
-ALTER TABLE `user_groupe`
-  ADD PRIMARY KEY (`user_id`,`groupe_id`),
-  ADD KEY `IDX_61EB971CA76ED395` (`user_id`),
-  ADD KEY `IDX_61EB971C7A45358C` (`groupe_id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `activit`
---
-ALTER TABLE `activit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT pour la table `groupe`
---
-ALTER TABLE `groupe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT pour la table `hebergement`
---
-ALTER TABLE `hebergement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT pour la table `trip`
---
-ALTER TABLE `trip`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `groupe`
---
-ALTER TABLE `groupe`
-  ADD CONSTRAINT `FK_4B98C2193B230BE` FOREIGN KEY (`admin_groupe_id`) REFERENCES `user` (`id`);
-
---
--- Contraintes pour la table `trip`
---
-ALTER TABLE `trip`
-  ADD CONSTRAINT `FK_7656F53B6236ABB4` FOREIGN KEY (`trip_hebergement_id`) REFERENCES `hebergement` (`id`),
-  ADD CONSTRAINT `FK_7656F53B675A74DB` FOREIGN KEY (`trip_groupe_id`) REFERENCES `groupe` (`id`),
-  ADD CONSTRAINT `FK_7656F53BCC7AB100` FOREIGN KEY (`trip_admin_id`) REFERENCES `user` (`id`);
-
---
--- Contraintes pour la table `trip_activit`
---
-ALTER TABLE `trip_activit`
-  ADD CONSTRAINT `FK_F1762FD9A5BC2E0E` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_F1762FD9BA71FC0C` FOREIGN KEY (`activit_id`) REFERENCES `activit` (`id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `user_groupe`
---
-ALTER TABLE `user_groupe`
-  ADD CONSTRAINT `FK_61EB971C7A45358C` FOREIGN KEY (`groupe_id`) REFERENCES `groupe` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_61EB971CA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
